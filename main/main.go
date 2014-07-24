8c141b1438d2519ef207c26faf72f345bf46967e
Walter Hop <walter@lifeforms.nl>
2014-07-24 21:59:26 +0200

    main: new Scenario struct format

--- main/main.go
+++ main/main.go
@@ -5,9 +5,11 @@ import "github.com/lifeforms/urlcheck/urlcheck"
 
 func main() {
 	sc := urlcheck.Scenario{
-		urlcheck.Test{Url: "http://sim.dt.lfms.nl/", Content: "sim"},
-		urlcheck.Test{Url: "http://www.lifeforms.nl/", Content: "DEHUMANIZE"},
-		urlcheck.Test{Url: "https://www.lifeforms.nl/", Content: "DEHUMANIZE"},
+		Tests: urlcheck.Tests{
+			urlcheck.Test{Url: "http://sim.dt.lfms.nl/", Content: "sim"},
+			urlcheck.Test{Url: "http://www.lifeforms.nl/", Content: "DEHUMANIZE"},
+			urlcheck.Test{Url: "https://www.lifeforms.nl/", Content: "DEHUMANIZo"},
+		},
 	}
 	err := sc.Test()
 	fmt.Println("Test result:", err)
