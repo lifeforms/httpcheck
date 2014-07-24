150a86549b7a109b81484c561859d230e997516d
Walter Hop <walter@lifeforms.nl>
2014-07-24 20:41:55 +0200

    basic url test

--- urlcheck/test.go
+++ urlcheck/test.go
@@ -1,6 +1,10 @@
 package urlcheck
 
+import "errors"
 import "fmt"
+import "net/http"
+import "strconv"
+import "strings"
 
 type Method int
 
@@ -19,7 +23,49 @@ type Test struct {
 	SkipSSLVerify bool
 }
 
-func (t Test) Test() error {
+func (t Test) Test() (err error) {
+	resp, err := t.DoRequest()
+	if err == nil {
+		err = t.CheckCode(resp)
+	}
+	return
+}
+
+func (t Test) DoRequest() (resp *http.Response, err error) {
 	fmt.Println("testing:", t.Url)
+	req, err := http.NewRequest(t.MethodName(), t.Url, strings.NewReader(t.Data))
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range t.Headers {
+		req.Header.Add(k, v)
+	}
+
+	resp, err = client.Do(req)
+	fmt.Println("response:", resp)
+	fmt.Println("error:", err)
+	return resp, err
+}
+
+func (t Test) CheckCode(resp *http.Response) error {
+	code := t.Code
+	if code == 0 {
+		code = 200
+	}
+	if resp.StatusCode != code {
+		return errors.New("Expected status code " + strconv.Itoa(code) + ", received " + strconv.Itoa(resp.StatusCode))
+	}
 	return nil
+}
+
+func (t Test) MethodName() string {
+	switch t.Method {
+	case GET:
+		return "GET"
+	case POST:
+		return "POST"
+	default:
+		return ""
+	}
 }
--- urlcheck/urlcheck.go
+++ urlcheck/urlcheck.go
@@ -1,7 +1,11 @@
 package urlcheck
 
+import "net/http"
+
 type Testable interface {
 	Test() error
 }
 
 type Manifest []Server
+
+var client = &http.Client{}
