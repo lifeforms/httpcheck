9565bab3b331d0dab0decb7e20ae997caaa2804f
Walter Hop <walter@lifeforms.nl>
2014-07-24 20:42:09 +0200

    main: add a failing test

--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import "fmt"
 import "github.com/lifeforms/urlcheck/urlcheck"
 
 func main() {
-	sim := urlcheck.Test{Url: "http://sim.dt.lfms.nl/"}
+	sim := urlcheck.Test{Url: "http://sim.dt.lfms.nl/", Code: 201}
 	lfms := urlcheck.Test{Url: "https://www.lifeforms.nl/"}
 	sc := urlcheck.Scenario{sim, lfms}
 	err := sc.Test()
