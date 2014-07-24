7009f391d83ac5989b4c496061d8ce4f4bcd7b1c
Walter Hop <walter@lifeforms.nl>
2014-07-24 21:38:02 +0200

    Scenario: change into struct so we can add Name attribute

--- urlcheck/scenario.go
+++ urlcheck/scenario.go
@@ -3,10 +3,15 @@ package urlcheck
 import "errors"
 import "strconv"
 
-type Scenario []Test
+type Tests []Test
+
+type Scenario struct {
+	Tests Tests
+	Name  string
+}
 
 func (s Scenario) Test() error {
-	for i, t := range s {
+	for i, t := range s.Tests {
 		err := t.Test()
 		if err != nil {
 			return errors.New("Step " + strconv.Itoa(i+1) + ": " + err.Error())
