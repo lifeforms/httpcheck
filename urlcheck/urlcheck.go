a5070864aba5b271d445e6eef283ee14639b93f4
Walter Hop <walter@lifeforms.nl>
2014-07-24 22:50:28 +0200

    init(): initialize 'client' with a HTTP client with a cookie jar

--- urlcheck/urlcheck.go
+++ urlcheck/urlcheck.go
@@ -1,11 +1,28 @@
 package urlcheck
 
-import "net/http"
+import (
+	"code.google.com/p/go.net/publicsuffix"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+)
 
-type Testable interface {
+type Tester interface {
 	Test() error
 }
 
 type Manifest []Server
 
-var client = &http.Client{}
+var client *http.Client
+
+// Set up a HTTP client with a cookie jar
+func init() {
+	options := cookiejar.Options{
+		PublicSuffixList: publicsuffix.List,
+	}
+	jar, err := cookiejar.New(&options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client = &http.Client{Jar: jar}
+}
