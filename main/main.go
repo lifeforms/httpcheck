544fd769485849f7cf1681962dbdd366569c160b
Walter Hop <walter@lifeforms.nl>
2014-07-24 21:20:40 +0200

    main: shorten

--- main/main.go
+++ main/main.go
@@ -4,9 +4,11 @@ import "fmt"
 import "github.com/lifeforms/urlcheck/urlcheck"
 
 func main() {
-	sim := urlcheck.Test{Url: "http://sim.dt.lfms.nl/", Code: 201}
-	lfms := urlcheck.Test{Url: "https://www.lifeforms.nl/"}
-	sc := urlcheck.Scenario{sim, lfms}
+	sc := urlcheck.Scenario{
+		urlcheck.Test{Url: "http://sim.dt.lfms.nl/", Content: "sim"},
+		urlcheck.Test{Url: "http://www.lifeforms.nl/", Content: "DEHUMANIZE"},
+		urlcheck.Test{Url: "https://www.lifeforms.nl/", Content: "DEHUMANIZE"},
+	}
 	err := sc.Test()
 	fmt.Println("Test result:", err)
 }
