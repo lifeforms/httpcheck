8b35e5a377079f64ebaabebf7ac2b6540bd3395d
Walter Hop <walter@lifeforms.nl>
2014-07-29 22:19:27 +0200

    parse command line arguments

--- main/main.go
+++ main/main.go
@@ -1,22 +1,43 @@
 package main
 
+import "flag"
 import "fmt"
 import "io/ioutil"
 import "github.com/lifeforms/urlcheck/urlcheck"
 
-func main() {
-	y, err := ioutil.ReadFile("urls.yaml")
+func parseArgs() (manifestfile string, verbose bool) {
+	flagi := flag.String("i", "urls.yaml", "Input file with YAML manifest")
+	flagv := flag.Bool("v", false, "Verbose, prints the result of each test")
+	flag.Parse()
+
+	verbose = *flagv
+	manifestfile = *flagi
+	return
+}
+
+func readManifest(manifestfile string) (manifest urlcheck.Manifest, err error) {
+	y, err := ioutil.ReadFile(manifestfile)
 	if err != nil {
-		fmt.Println("Error reading:", err)
-		return
-	}
-	manifest, err := urlcheck.FromYAML(y)
-	if err != nil {
-		fmt.Println("Error parsing:", err)
-		return
+		return nil, err
 	}
 
-	err = manifest.Test()
+	manifest, err = urlcheck.FromYAML(y)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func main() {
+	manifestfile, verbose := parseArgs()
+
+	urlcheck.Verbose = verbose
+	manifest, err := readManifest(manifestfile)
+	if err == nil {
+		err = manifest.Test()
+	}
+
 	if err != nil {
 		fmt.Println("Failures:", err)
 	} else {
